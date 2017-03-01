f155f3f94e8c34c7cbc379bc2ad2ebaea0107ae8
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-01 10:50:24 +0100

    The 'let' keyword was not recognized in slave

--- calc.sh
+++ calc.sh
@@ -1,6 +1,6 @@
 alderspensionsavgift=$(cat target/alderspensionsavgift.txt)
 allman_pensionsavgift=$(cat target/allman_pensionsavgift.txt)
 arbetsgivaravgift_och_egenavgift=$(cat target/arbetsgivaravgift_och_egenavgift.txt)
-let res="${alderspensionsavgift} + ${allman_pensionsavgift} + ${arbetsgivaravgift_och_egenavgift}"
+res=$(echo "scale=0; (${alderspensionsavgift} + ${allman_pensionsavgift} + ${arbetsgivaravgift_och_egenavgift}) / 1" | bc -l)
 echo ${res} > target/pensionsratter.txt
 
