484c3873e4fcf731a37956f086e7859017ed1084
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-02 10:58:36 +0100

    Using separate input and output directories

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # gocd-pension-pensionsratter
 Beräkningar av pensionsrätter
 
+Förväntar sig indata i katalogen "input" och lägger resultatet i katalogen "output".
+
 ## Innehåll
 Kod för att beräkna:
 * Summering av pensionsrätter
--- calc.sh
+++ calc.sh
@@ -1,6 +1,9 @@
-alderspensionsavgift=$(cat target/alderspensionsavgift.txt)
-allman_pensionsavgift=$(cat target/allman_pensionsavgift.txt)
-arbetsgivaravgift_och_egenavgift=$(cat target/arbetsgivaravgift_och_egenavgift.txt)
+#!/usr/bin/env bash
+set -e
+mkdir -p output
+alderspensionsavgift=$(cat input/alderspensionsavgift.txt)
+allman_pensionsavgift=$(cat input/allman_pensionsavgift.txt)
+arbetsgivaravgift_och_egenavgift=$(cat input/arbetsgivaravgift_och_egenavgift.txt)
 res=$(echo "scale=0; (${alderspensionsavgift} + ${allman_pensionsavgift} + ${arbetsgivaravgift_och_egenavgift}) / 1" | bc -l)
-echo ${res} > target/pensionsratter.txt
+echo ${res} > output/pensionsratter.txt
 
