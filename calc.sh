c2c99c854ca13cc5b83345d2617bb1bdabb50336
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-02 15:22:16 +0100

    Use specific output directory and generic file name

--- calc.sh
+++ calc.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
-mkdir -p output
+mkdir -p output/pensionsratter
 alderspensionsavgift=$(cat input/alderspensionsavgift.txt)
 allman_pensionsavgift=$(cat input/allman_pensionsavgift.txt)
 arbetsgivaravgift_och_egenavgift=$(cat input/arbetsgivaravgift_och_egenavgift.txt)
 res=$(echo "scale=0; (${alderspensionsavgift} + ${allman_pensionsavgift} + ${arbetsgivaravgift_och_egenavgift}) / 1" | bc -l)
-echo ${res} > output/pensionsratter.txt
+echo ${res} > output/pensionsratter/res.txt
 
