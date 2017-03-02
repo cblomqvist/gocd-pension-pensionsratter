dd9a276d5c5356f14d42396e5ee59873f1f0c9ce
Christer Blomqvist <christer.blomqvist@pensionsmyndigheten.se>
2017-03-02 17:17:11 +0100

    Revert to simpler file structure

--- calc.sh
+++ calc.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -e
 mkdir -p output/pensionsratter
-alderspensionsavgift=$(cat input/alderspensionsavgift.txt)
-allman_pensionsavgift=$(cat input/allman_pensionsavgift.txt)
-arbetsgivaravgift_och_egenavgift=$(cat input/arbetsgivaravgift_och_egenavgift.txt)
+INPUT=upstream/output
+alderspensionsavgift=$(cat ${INPUT}/alderspensionsavgift.txt)
+allman_pensionsavgift=$(cat ${INPUT}/allman_pensionsavgift.txt)
+arbetsgivaravgift_och_egenavgift=$(cat ${INPUT}/arbetsgivaravgift_och_egenavgift.txt)
 res=$(echo "scale=0; (${alderspensionsavgift} + ${allman_pensionsavgift} + ${arbetsgivaravgift_och_egenavgift}) / 1" | bc -l)
-echo ${res} > output/pensionsratter/res.txt
+echo ${res} > output/pensionsratter.txt
 
